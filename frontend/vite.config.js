7550f5e8e5a9722bf2c0326fb884e51d53e2949b
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2025-11-13 21:48:18 +0530

    Maked initial files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,  // 👈 this auto-opens the app in your default browser
+    port: 5173,  // optional, you can set a custom port too
+  },
 })
